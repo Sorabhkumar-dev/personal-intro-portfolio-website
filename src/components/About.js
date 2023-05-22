0658795e3288c834efa5119d31aabcd4f06f7a5f
Sorabh kumar <96188190+Sorabhkumar-dev@users.noreply.github.com>
2023-05-22 10:44:18 +0530

    chore:completed about us pae

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import '../css/About.css'
+import MainTopHeading from '../components/MainTopHeading'
+import AboutTopIntroCard from '../components/AboutTopIntroCard'
+import AboutIndustriesSection from './AboutIndustriesSection'
 
 export default function About() {
   return (
-    <div>
-      <h1>About page</h1>
+    <div className='about_main_conatiner'>
+      <MainTopHeading primaryHeading = "About " secondoryHeading = "Us!"/>
+      <AboutTopIntroCard/>
+      <AboutIndustriesSection/>
     </div>
   )
 }
---
+++ src/components/AboutIndustriesSection.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PrimaryHeading from './PrimaryHeading'
+import '../css/AboutIndustriesSection.css'
+import IndustryCard from './IndustryCard'
+import AI_ML_icon from '../asset/icon/AI_ML.svg'
+import digitalPlatformIcon from '../asset/icon/digital_platforms.svg'
+import fintechIcon from '../asset/icon/fintech.svg'
+import semiconducterIcon from '../asset/icon/semiconductor.svg'
+import holdingIcon from '../asset/icon/holdings.svg'
+// import iotIcon from '../asset/icon/iot.svg'
+
+
+export default function AboutIndustriesSection() {
+    const industries = [
+        {industry:"Semiconducter", icon:{semiconducterIcon}},
+        {industry:"AI/ML", icon:{AI_ML_icon}},
+        {industry:"Digital Platforms", icon:{digitalPlatformIcon}},
+        {industry:"Faster Holdings", icon:{holdingIcon}},
+        // {industry:"IOT", icon:{iotIcon}},
+        {industry:"Fintech", icon:{fintechIcon}}
+    ]
+    
+    const industriesIcons = industries.map(data => {
+       return <IndustryCard icon = {semiconducterIcon} industryName ={data.industry} />;
+    });
+
+  return (
+    <div class = "about_industries_containetr">
+      <PrimaryHeading primaryHeading = "Industries which" secondoryHeading = "We are operating!"/>
+      <div className="industries_list">
+        {industriesIcons}
+      </div>
+    </div>
+  )
+}
